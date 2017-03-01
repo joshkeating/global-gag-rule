b180797e851ac8c73b24ff1d406dbe3b3aa178da
joshkeating <josh.keating7@gmail.com>
2017-03-01 10:21:24 +0000

    messing around with shiny and .md

---
+++ summary.md
@@ -0,0 +1,11 @@
+# Global Gag Rule
+
+By Peter Freschi, Josh Keating, and John Harrison
+
+_A tool for analysing the global public health effects of the Mexico City policy_
+
+***
+
+#### Project Description
+
+Talk about what we're going to do.
--- ui.R
+++ ui.R
@@ -1,10 +1,19 @@
 
 library(shiny)
 library(dplyr)
+library(markdown)
 
 navbarPage("Global Gag Rule",
            
-           tabPanel("Summary"),
+           tabPanel("Summary",
+                    
+                    fluidPage(
+                      
+                      includeMarkdown("summary.md")
+                      
+                    )
+                    
+           ),
            
            tabPanel("Viz 1"
            
