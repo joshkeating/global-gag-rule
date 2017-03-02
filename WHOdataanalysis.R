f30746cf37ccfc42f5bb1084e9863e039fa6ca7c
joshkeating <josh.keating7@gmail.com>
2017-03-02 19:33:16 +0000

    getting close to mm and ab comparison close #2

--- WHOdataanalysis.R
+++ WHOdataanalysis.R
@@ -6,10 +6,11 @@
 
 library(dplyr)
 library(ggplot2)
+library(tidyr)
 
 who.data <- read.csv("data/final-who.csv")
 # mm.data <- read.csv("data/maternal-mortality-WHO.csv")
-new.mm.data <- read.csv("data/Maternal-Mortality-1994-2008.csv")
+new.mm.data <- read.csv("data/Maternal-Mortality-1994-2008.csv", check.names = FALSE)
 
 just.high <- who.data %>% filter(Eposure.to.Meico.City.Policy == "High")
 
@@ -36,8 +37,26 @@ all.plot <- ggplot(just.high, aes(x=Year)) +
 
 all.plot
 
+sub.countries <- just.high %>% distinct(Country)
+
+# new.mm.data <- new.mm.data %>% rename(Country = Country Name)
+
+# this is for the sake of time, I know it looks gross
+just.sub.saharan <- new.mm.data %>% filter(Country == "Benin" | Country == "Ghana" |Country == "Guinea" |
+                                             Country == "Madagascar" |Country == "Malawi" |Country == "Mali" |
+                                             Country == "Mozambique" |Country == "Tanzania" |Country == "Benin")
 
 
+
+# 
+# all.plot <- ggplot(just.sub.saharan, aes(x=Year)) +
+#   geom_smooth(method = "loess", aes(x=Year, y=Num.Ab)) +
+#   geom_point(aes(x=Year, y=Num.Ab, colour=Country)) +
+#   labs(x = "Year", y = "abor") +
+#   ggtitle("title") + 
+#   geom_vline(xintercept = 2001)
+# 
+# all.plot
 
 
 
