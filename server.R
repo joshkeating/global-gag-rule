ca2ae95997a6c823014ab1ed4649703383568bc6
joshkeating <josh.keating7@gmail.com>
2017-03-05 21:53:03 +0000

    well heres something

--- server.R
+++ server.R
@@ -8,5 +8,21 @@ data <- read.csv("./data/sub-saharan-exposure-and-maternal-mortality", stringsAs
 function(input, output, session) {
   
   
+  p <- ggplot(data, aes(x=Year)) +
+    geom_smooth(method = "loess", aes(x=Year, y=Num.Ab, colour = Country, fill = Country)) +
+    geom_point(aes(x=Year, y=Num.Ab, colour=Country)) +
+    labs(x = "Year", y = "abor") +
+    ggtitle("title") + 
+    geom_vline(xintercept = 2001)
   
+  
+  
+  
+  
+  output$plot <- renderPlotly({
+    # this is a plot
+    
+    ggplotly(p)
+  })
+  # 
 }
--- ui.R
+++ ui.R
@@ -3,6 +3,8 @@ library(shiny)
 library(dplyr)
 library(markdown)
 
+data <- read.csv("./data/sub-saharan-exposure-and-maternal-mortality", stringsAsFactors = FALSE)
+
 navbarPage("Global Gag Rule",
            
            tabPanel("Summary",
@@ -15,8 +17,29 @@ navbarPage("Global Gag Rule",
                     
            ),
            
-           tabPanel("Viz 1"
+           tabPanel("Viz 1",
            
+                    fluidPage(
+                      
+                      titlePanel("Test"),
+                      
+                      
+                      plotlyOutput("plot")
+                      
+                      
+                      
+                      # sidebarPanel(
+                      #   
+                      #   # creates select dropdown widget
+                      #   selectInput("select", label = h3("Select Country"), 
+                      #               choices = c(unique(as.character(data$Country))),
+                      #               selected = "Benin"),
+                      #   
+                      #   helpText("Select something")
+                      #   
+                      # )
+                      
+                    )
                     
            ),
            
