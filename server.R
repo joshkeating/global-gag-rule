58a751e1c2ee1e0c9ba73a76f026c2f8dc238fb9
joshkeating <josh.keating7@gmail.com>
2017-03-06 23:11:58 +0000

    fixed broken things

--- server.R
+++ server.R
@@ -8,8 +8,8 @@ UN_data <- read.csv('./data/UN.csv', na.strings="..", stringsAsFactors = FALSE)
 aid_data <- read.csv('data/foreign-aid.csv', stringsAsFactors = FALSE)
 colnames(WHO_data)[5] <- "Number_Abortions"
 
-WHO.just.high <- WHO.data %>% filter(Exposure == "High")
-WHO.just.low <- WHO.data %>% filter(Exposure == "Low")
+WHO.just.high <- WHO_data %>% filter(Exposure == "High")
+WHO.just.low <- WHO_data %>% filter(Exposure == "Low")
 
 UNyOpts1 <- c("CP_Any_Method", "CP_Any_Modern_Method", "CP_Any_Traditional_Method", "UN_Unmet_Need_Total", "Demand_Satisfied_By_Modern_Methods")
 names(UNyOpts1) <- c("Contraceptive Prevalence", "Contraceptive (Modern) Prevalence", "Contraceptive (Traditional) Prevalence", "% with Unmet Needs for Family Planning", "Proportion of Family Planning Demand Satisfied via Modern Methods")
@@ -45,14 +45,14 @@ function(input, output, session) {
     geom_vline(xintercept = 2001, linetype="dotted") + 
     theme_bw()
   
-  Who.by.country <- ggplot(WHO.data) +
+  Who.by.country <- ggplot(WHO_data) +
     geom_smooth(method = "loess", se = FALSE, aes(x=Year, y=Number_Abortions, colour = Country)) + 
     labs(x = "Year", y = "Induced Abortion") + 
     ggtitle("Induced Abortion In Selected Sub-Saharan African Countries") + 
     geom_vline(xintercept = 2001, linetype="dotted") + 
     theme_bw()
   
-  Who.by.mm <- ggplot(WHO.data) + 
+  Who.by.mm <- ggplot(WHO_data) + 
     geom_smooth(method = "loess", aes(x=Year, y=Maternal_Mortality)) + 
     geom_jitter(aes(x=Year, y=Maternal_Mortality, colour=Country)) + 
     labs(x = "Year", y = "Maternal Mortality per 100,000") + 
