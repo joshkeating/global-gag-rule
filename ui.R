5f3ae4a3f35ee0c2844837395df434199f54aadb
joshkeating <josh.keating7@gmail.com>
2017-03-06 21:20:53 +0000

    fixed UNyOpts1 error

--- ui.R
+++ ui.R
@@ -3,6 +3,11 @@ library(shiny)
 library(dplyr)
 library(plotly)
 
+UNyOpts1 <- c("CP_Any_Method", "CP_Any_Modern_Method", "CP_Any_Traditional_Method", "UN_Unmet_Need_Total", "Demand_Satisfied_By_Modern_Methods")
+names(UNyOpts1) <- c("Contraceptive Prevalence", "Contraceptive (Modern) Prevalence", "Contraceptive (Traditional) Prevalence", "% with Unmet Needs for Family Planning", "Proportion of Family Planning Demand Satisfied via Modern Methods")
+countries <- c("Angola", "Burundi", "Cabo Verde", "Central African Republic", "Chad", "Comoros", "Djibouti", "Equatorial Guinea", "Eritrea", "Gabon", "Gambia", "Guinea-Bissau", "Liberia", "Mali", "Mauritania", "Sao Tome and Principe", "the Seychelles", "Somalia", "South Sudan", "Sudan", "Zimbabwe")
+
+
 navbarPage("Global Gag Rule",
            
            tabPanel("Summary", 
