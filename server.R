cb9776857ba9659989e3e62e0c127857d4de9060
Peter Freschi <pfreschi@uw.edu>
2017-03-06 20:41:47 -0800

    added dotted line and rearranged

--- server.R
+++ server.R
@@ -7,25 +7,25 @@ WHO_data <- read.csv("./data/sub-saharan-exposure-and-maternal-mortality", strin
 UN_data <- read.csv('./data/UN.csv', na.strings="..", stringsAsFactors = FALSE)
 colnames(WHO_data)[5] <- "Number_Abortions"
 
+UNyOpts1 <- c("CP_Any_Method", "CP_Any_Modern_Method", "CP_Any_Traditional_Method", "UN_Unmet_Need_Total", "Demand_Satisfied_By_Modern_Methods")
+names(UNyOpts1) <- c("Contraceptive Prevalence", "Contraceptive (Modern) Prevalence", "Contraceptive (Traditional) Prevalence", "% with Unmet Needs for Family Planning", "Proportion of Family Planning Demand Satisfied via Modern Methods")
+countries <- c("Angola", "Burundi", "Cape Verde", "Central African Republic", "Chad", "Comoros", "Djibouti", "Equatorial Guinea", "Eritrea", "Gabon", "Gambia", "Guinea-Bissau", "Liberia", "Mali", "Mauritania", "Sao Tome and Principe", "the Seychelles", "Somalia", "South Sudan", "Sudan", "Zimbabwe")
+
+
 function(input, output, session) {
   
   
-  countries <- c("Angola", "Burundi", "Cape Verde", "Central African Republic", "Chad", "Comoros", "Djibouti", "Equatorial Guinea", "Eritrea", "Gabon", "Gambia", "Guinea-Bissau", "Liberia", "Mali", "Mauritania", "Sao Tome and Principe", "the Seychelles", "Somalia", "South Sudan", "Sudan", "Zimbabwe")
   
   africanData <- UN_data[UN_data$Country_Area %in% countries,] %>% filter(Population == "MW")
-  
+  Country <- factor(africanData$Country_Area)
   output$UNplot1 <- renderPlotly({
-    Country <- factor(africanData$Country_Area)
     
-    UNyOpts1 <- c("CP_Any_Method", "CP_Any_Modern_Method", "CP_Any_Traditional_Method", "UN_Unmet_Need_Total", "Demand_Satisfied_By_Modern_Methods")
-    names(UNyOpts1) <- c("Contraceptive Prevalence", "Contraceptive (Modern) Prevalence", "Contraceptive (Traditional) Prevalence", "% with Unmet Needs for Family Planning", "Proportion of Family Planning Demand Satisfied via Modern Methods")
     Selected_Variable <- select(africanData, one_of(UNyOpts1[[input$y]]))
     
     UNviz1 <- ggplot(africanData, aes(x=Survey_End_Year, y = Selected_Variable, group=Country_Area)) + 
       geom_line(aes(colour= Country))+
       geom_point(aes(colour = Country))+
-      annotate("rect", xmin = 1984, xmax = 1993, ymin=-Inf, ymax=Inf, alpha = .15)+
-      annotate("rect", xmin = 2001, xmax = 2009, ymin=-Inf, ymax=Inf, alpha = .15)+
+      geom_vline(xintercept = c(1984, 1993, 2001, 2009), linetype="dotted")
       labs(title = "Married/In-Union Women", x = "Year", y = input$y)
 
     ggplotly(UNviz1)
