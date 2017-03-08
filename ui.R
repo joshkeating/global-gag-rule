18f1bb5369a0d75b4d6c8bf00b662dfd966b1df5
John Laws Harrison <johnlawsharrison@gmail.com>
2017-03-08 07:23:25 -0800

    Add link

--- ui.R
+++ ui.R
@@ -182,18 +182,21 @@ navbarPage("Global Gag Rule",
                       # what I would give for an ng-repeat right now
                       conditionalPanel(
                         condition = "input.ngo == 'John Snow International'",
-                        h2("More from this NGO"),
+                        h3("More from this NGO"),
                         br(),
                         tags$blockquote(
                           "\"It’s going to be harder for women particularly to have HIV testing and quick, immediate referrals for ARVs. Right now, we have a program in Zambia where women are referred for HIV testing and can get ARVs all in one single day. We are now potentially going to make this far more complicated for them, and there will be far more opportunities for people to drop out.\"",
                           br(),
                           a(href="http://jsi.com/JSIInternet/Newsroom/newsitem/display.cfm?a=1&id=2029", "- Andrew Fullem, director of the HIV and Infectious Diseases Center at John Snow, Inc")
                         ),
-                        tags$blockquote("\"JSI stands with more than 140 partners in opposition to the U.S.-imposed Global Gag Rule, a policy that will leave millions of women and their families in the developing world without access to the critical health services that they need.\"")
+                        tags$blockquote("\"JSI stands with more than 140 partners in opposition to the U.S.-imposed Global Gag Rule, a policy that will leave millions of women and their families in the developing world without access to the critical health services that they need.\""),
+                        tags$ul(
+                          tags$li(a(href="https://www.refugeesinternational.org/advocacy-letters-1/global-gag-rule", "Endorses the Coalition Statement on Opposing the Gag Rule")))
+                        )
                       ),
                       conditionalPanel(
                         condition = "input.ngo == 'EngenderHealth'",
-                        h2("More from this NGO"),
+                        h3("More from this NGO"),
                         br(),
                         tags$blockquote(
                           "\"It is a fundamental right for all women and girls to be allowed access to comprehensive sexual and reproductive health care. The Global Gag Rule effectively strips them of this right to make decisions in their private lives that will empower them to lift themselves out of poverty and enable them to live up to their full potential\"",
@@ -203,7 +206,7 @@ navbarPage("Global Gag Rule",
                       ),
                       conditionalPanel(
                         condition = "input.ngo == 'Pathfinder International'",
-                        h2("More from this NGO"),
+                        h3("More from this NGO"),
                         br(),
                         tags$blockquote(
                           "\"Imposing the global gag rule will force many organizations and clinicians to disregard their local laws and deny women rights that are legally theirs. Stopping organizations from speaking freely about abortion in their own countries is counter to the American commitment to free speech and promoting democracy.\"",
@@ -213,7 +216,7 @@ navbarPage("Global Gag Rule",
                       ),
                       conditionalPanel(
                         condition = "input.ngo == 'Marie Stopes International'",
-                        h2("More from this NGO"),
+                        h3("More from this NGO"),
                         br(),
                         tags$blockquote(
                           "\"All the medical evidence, as well as everything we know from our daily interactions with women, is unequivocal: if you take safe abortion services out of the reproductive healthcare package, it exposes women to risk.\"",
@@ -223,7 +226,7 @@ navbarPage("Global Gag Rule",
                       ),
                       conditionalPanel(
                         condition = "input.ngo == 'IntraHealth International'",
-                        h2("More from this NGO"),
+                        h3("More from this NGO"),
                         br(),
                         tags$blockquote("\"Our mission is to improve the performance of health workers and strengthen the systems in which they work. Our vision is that everyone, everywhere has the health care they need to thrive.\""),
                         br(),
@@ -234,7 +237,7 @@ navbarPage("Global Gag Rule",
                       ),
                       conditionalPanel(
                         condition = "input.ngo == 'Management Sciences for Health'",
-                        h2("More from this NGO"),
+                        h3("More from this NGO"),
                         br(),
                         tags$blockquote(
                           "\"Reinstating the Global Gag Rule cripples health systems that use U.S. funds for comprehensive reproductive health care, and will shut down essential programs that increase access to services for HIV/AIDS and gender-based violence. The Global Gag Rule violates the right of all women to access comprehensive sexual and reproductive health care and to make informed choices about their health.\"",
@@ -249,7 +252,7 @@ navbarPage("Global Gag Rule",
                       ),
                       conditionalPanel(
                         condition = "input.ngo == 'Population Council'",
-                        h2("More from this NGO"),
+                        h3("More from this NGO"),
                         br(),
                         tags$blockquote(
                           "\"The Mexico City Policy will have a negative effect on the lives of millions of girls, women and families around the world and will impede countless global health projects and programs\"",
@@ -264,7 +267,7 @@ navbarPage("Global Gag Rule",
                       ),
                       conditionalPanel(
                         condition = "input.ngo == 'Population Services International'",
-                        h2("More from this NGO"),
+                        h3("More from this NGO"),
                         br(),
                         tags$ul(
                           tags$li(a(href="http://www.psi.org/president-trump-threatens-womens-health-vip-conference-call/", "Public conference call denouncing the global gag rule")),
